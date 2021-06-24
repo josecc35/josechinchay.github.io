a17f81e6216428cec8509f32a6962e0849e0f3b4
josechinchay <robertchinchay8@gmail.com>
2021-06-24 00:50:59 -0500

    vv

---
+++ google.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+  var coord = {lat:-12.064260, lng:   -77.041793};  
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 18,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+}
